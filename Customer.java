f0908ea4318781197c01eab150f357083ff3ab11
JJ Chang (he/him) <75114824+Jayz-lab@users.noreply.github.com>
2023-01-23 16:55:27 +1100

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,16 @@
+package duke.choice;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author JJ
+ */
+public class Customer {
+    String name;
+    String size;
+    
+}
