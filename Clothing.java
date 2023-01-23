7588ab2ff6e86e2caaf5731192ebe1413159fa2b
JJ Chang (he/him) <75114824+Jayz-lab@users.noreply.github.com>
2023-01-23 16:57:01 +1100

    Create Clothing.java

---
+++ Clothing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package duke.choice;
+
+/**
+ *
+ * @author JJ
+ */
+public class Clothing {
+    String description;
+    double price;
+    String size = "M";
+    
+    
+}
