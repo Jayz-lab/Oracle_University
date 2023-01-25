c088b83de062fe1fb1b0df420f74ca2dc733573f
JJ Chang (he/him) <75114824+Jayz-lab@users.noreply.github.com>
2023-01-25 11:55:15 +1100

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -14,3 +14,38 @@ public class Customer {
     String size;
     
 }
+/*
+Modeling Classes
+- Objects & Classess: Objs are based on Classes, Classess are used for design and objs are made during runtime.
+- analysis according to use cases.
+
+Modeling Properties/Attributes and Behaviours
+- Classes are modeled using Unified Modelling Language(UML)diagrams.
+- UML model would contain descriptions of datd types, method, parameters and so on.
+
+Benefits of Modeling is that it prevents future costly and time consumer remodelling of the app/program.
+Modeling helps to get close to the ideal of the business needs and requiremeents.
+
+*/
+
+//Define Methods
+//components of a method definition
+/*
+<access modifier- private, public.etc> <return type- void, double, int.etc> <method name> () {
+        <method body>
+        <return statement> 
+}
+
+//in code
+public void printWishlist () {
+    //method body
+    String[] list = {"Golden hat", "Iron Boots";
+for (String idea: list) {
+    System.out.println(idea);
+    }
+}
+public double getTotal(double price, int quality) {
+    double total = price*quantity*tax;
+    return total;
+}
+*/
